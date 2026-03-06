998a5441061c2ef10fbcaf986fe7021cd1ee0263
Christopher1078 <christortizfun@gmail.com>
2026-03-06 09:44:42 -0600

    Arreglo de bug

--- Editor/src/editor/Editor.java
+++ Editor/src/editor/Editor.java
@@ -14,7 +14,8 @@ public class Editor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+            GUI ventana = new GUI();
+            ventana.setVisible(true);    }
     
 }
