480dad7bfde9e21657f04ed397735f3370ddf441
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-03-06 11:40:09 -0600

    n

--- Editor/src/editor/DocxTableManager.java
+++ Editor/src/editor/DocxTableManager.java
@@ -39,9 +39,9 @@ public class DocxTableManager {
 
         for (int c = 0; c < encabezados.length; c++) {
             if (c == 0) {
-               // table.getRow(0).getCell(0).setText(encabezados[0]);
+                table.getRow(0).getCell(0).setText(encabezados[0]);
             } else {
-               // table.getRow(0).addNewTableCell().setText(encabezados[c]);
+                table.getRow(0).addNewTableCell().setText(encabezados[c]);
             }
         }
 
