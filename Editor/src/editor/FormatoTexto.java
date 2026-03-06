b0c5ee2ae7199b9bb6b72e84a469f12e35c12653
Christopher1078 <christortizfun@gmail.com>
2026-03-06 11:56:18 -0600

    Arreglo en errores

--- Editor/src/editor/FormatoTexto.java
+++ Editor/src/editor/FormatoTexto.java
@@ -1,66 +1,72 @@
 package editor;
 
 import javax.swing.JTextPane;
-import javax.swing.text.StyleConstants;
 import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 import java.awt.Color;
 
 public class FormatoTexto {
+
     private JTextPane panel;
     private StyledDocument docx;
 
+    private SimpleAttributeSet atributosActivos = new SimpleAttributeSet();
+
     public FormatoTexto(JTextPane panel) {
         this.panel = panel;
-        this.docx=panel.getStyledDocument();
+        this.docx = panel.getStyledDocument();
     }
-    
-    public void cambiarFuente(String fuente){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setFontFamily(atributos, fuente);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+    private void aplicar(SimpleAttributeSet atributos) {
+        int inicio = panel.getSelectionStart();
+        int fin    = panel.getSelectionEnd();
+
+        if (inicio != fin) {
+            docx.setCharacterAttributes(inicio, fin - inicio, atributos, false);
+        }
+
+        atributosActivos.addAttributes(atributos);
+        panel.setCharacterAttributes(atributosActivos, false);
     }
-    
-    public void cambiarTamano(int tamano){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setFontSize(atributos, tamano);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+
+    public void cambiarFuente(String fuente) {
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setFontFamily(a, fuente);
+        aplicar(a);
     }
-    
-    public void cambiarColor(Color color){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setForeground(atributos, color);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+    public void cambiarTamano(int tamano) {
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setFontSize(a, tamano);
+        aplicar(a);
     }
-    
-    public void ponerNegrita(){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setBold(atributos, true);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+    public void cambiarColor(Color color) {
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setForeground(a, color);
+        aplicar(a);
     }
-    
-    public void ponerCursiva(){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setItalic(atributos, true);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+    public void ponerNegrita() {
+        boolean negritaActual = StyleConstants.isBold(atributosActivos);
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setBold(a, !negritaActual);
+        aplicar(a);
     }
-    
-    public void ponerSubrayado(){
-        int inicio=panel.getSelectionStart();
-        int fin=panel.getSelectionEnd();
-        SimpleAttributeSet atributos=new SimpleAttributeSet();
-        StyleConstants.setUnderline(atributos, true);
-        docx.setCharacterAttributes(inicio, fin, atributos, false);
+
+    public void ponerCursiva() {
+        boolean cursivaActual = StyleConstants.isItalic(atributosActivos);
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setItalic(a, !cursivaActual);
+        aplicar(a);
     }
-    
+
+    public void ponerSubrayado() {
+        boolean subActual = StyleConstants.isUnderline(atributosActivos);
+        SimpleAttributeSet a = new SimpleAttributeSet();
+        StyleConstants.setUnderline(a, !subActual);
+        aplicar(a);
+    }
 }
